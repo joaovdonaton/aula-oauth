7375a238dd12e86a16df067a15926483cc94da66
joaovdonaton <joao.vitor.donaton@gmail.com>
2023-04-18 18:58:38 -0300

    a

---
+++ src/main/java/br/pucpr/aulaoauth/AulaOauthApplication.java
@@ -0,0 +1,13 @@
+package br.pucpr.aulaoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaOauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AulaOauthApplication.class, args);
+    }
+
+}
